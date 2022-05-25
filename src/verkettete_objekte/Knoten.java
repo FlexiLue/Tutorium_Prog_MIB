359fb1dec6524a83763343c86962d77397b6dd10
Felix <felix.luette@web.de>
2022-05-25 13:47:03 +0200

    Update verkettete_objekte

--- src/verkettete_objekte/Knoten.java
+++ src/verkettete_objekte/Knoten.java
@@ -1,37 +1,49 @@
 package verkettete_objekte;
 
 public class Knoten {
-	int x;
+	int wert;
 	Knoten links; 
 	Knoten rechts;
 	
-	public Knoten(int x) {
-		this.x = x;
+	public Knoten(int wert) {
+		this.wert = wert;
 		this.links = null; 
 		this.rechts = null;
 	}
 	
 	public void einfuegen(int n) {
-		Knoten newKnoten = new Knoten(n);
-		Knoten focusKnoten = this;
-		Knoten parentKnoten;
+		Knoten knoten = new Knoten(n);
 		
-		while(true) {
-			parentKnoten = focusKnoten;
-			if(n < focusKnoten.x) {
-				focusKnoten = focusKnoten.links;
-				if(focusKnoten == null) {
-					parentKnoten.links = newKnoten;
-					return;
-				}
+		if(n > wert) {
+			if(rechts != null) {
+				rechts.einfuegen(n);
 			} else {
-				focusKnoten = focusKnoten.rechts;
-				if(focusKnoten == null) {
-					parentKnoten.rechts = newKnoten;
-					return;
-				}
+				rechts = knoten;
+				return;
+			}
+		} else if (n < wert) {
+			if(links != null) {
+				links.einfuegen(n);
+			} else {
+				links = knoten;
+				return;
 			}
 		}
-		
+	}
+	
+	public String toString() {
+		if(links == null && rechts == null ) {
+			return "(" + wert +")";
+		}
+		if(links != null && rechts != null) {
+			return "(" + links.toString() + wert + rechts.toString() + ")";
+		}
+		if(rechts != null) {
+			return "" + wert + rechts.toString() + ")";
+		}
+		if(links != null) {
+			return "(" + links.toString() + wert + ")";
+		}
+		return ""; 
 	}
 }
--- src/verkettete_objekte/KnotenTester.java
+++ src/verkettete_objekte/KnotenTester.java
@@ -3,13 +3,15 @@ package verkettete_objekte;
 public class KnotenTester {
 
 	public static void main(String[] args) {
-		Knoten knoten1 = new Knoten(1);
-		knoten1.einfuegen(7);
+		Knoten knoten1 = new Knoten(3);
+		knoten1.einfuegen(1);
 		knoten1.einfuegen(2);
 		knoten1.einfuegen(6);
 		knoten1.einfuegen(5);
-		knoten1.einfuegen(11);
-		knoten1.einfuegen(9);
+//		knoten1.einfuegen(11);
+//		knoten1.einfuegen(9);
+		
+		System.out.println(knoten1.toString());
 	}
 
 }
